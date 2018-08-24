2153f5461959ef63583405c327329e6f04be90e1
Arnyxa <arnyxxa@gmail.com>
2018-08-24 14:14:07 +0200

    hhhh

--- Nyxa3D/Callback.h
+++ Nyxa3D/Callback.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <memory>
+
+namespace nx
+{
+	class Callbacks
+	{
+	public:
+		typedef std::unique_ptr<Callbacks> ptr;
+
+		virtual void Execute() = 0;
+	};
+
+	template<typename T>
+	class Call : public Callbacks
+	{
+	public:
+		Call(void (T::*aFunction)(), T* anObjPtr)
+			: mFunction(aFunction), mObject(anObjPtr)
+		{}
+
+		void Execute() override
+		{ (mObject->*mFunction)(); }
+
+	private:
+		void(T::*mFunction)();
+		T* mObject;
+	};
+}
